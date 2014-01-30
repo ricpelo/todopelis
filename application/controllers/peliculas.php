df4ca395ecb7a24dad62d110c8f2c481cfe7e62b
Jose Palacios <caosmaster88@gmail.com>
2014-01-30 17:09:47 +0100

    Creacion de ficheros para los controladores

---
+++ application/controllers/peliculas.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Controlador de fichas
+ */
+class Fichas extends CI_Controller 
+{
+	
+	function __construct($argument) 
+	{
+		
+	}
+}
