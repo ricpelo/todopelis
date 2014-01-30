35e4d1ae2dfea96f34bbff739a41b076289ff8a4
José María Romero Raposo <jmaria.romero@gmail.com>
2014-01-30 16:52:05 +0100

    configuracion inicial de codeIgniter

--- application/config/database.php
+++ application/config/database.php
@@ -49,10 +49,10 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['username'] = 'todopelis';
+$db['default']['password'] = 'todopelis';
+$db['default']['database'] = 'todopelis';
+$db['default']['dbdriver'] = 'postgre';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
