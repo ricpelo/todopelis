ac4ea40e606cb70d51600a47071607705720c2e3
Jose Palacios <caosmaster88@gmail.com>
2014-02-03 16:03:42 +0100

    Metodo ficha_de en controlador

--- application/controllers/peniculas.php
+++ application/controllers/peniculas.php
@@ -10,4 +10,25 @@ class Fichas extends CI_Controller
 	{
 		
 	}
+  
+  function ficha_de($id_penicula = null)
+  {
+    if ($id_penicula == null) throw new Exception("Película incorrecta");
+    
+    $datos = $this->Penicula->obtener_datos($id_penicula);
+    $directores = $this->Penicula->obtener_directores($id_penicula);
+    $reparto = $this->Penicula->obtener_reparto($id_penicula);
+    $generos = $this->Penicula->obtener_generos($id_penicula);
+    $paises = $this->Penicula->obtener_paises($id_penicula);
+    
+    $data['datos'] = $datos;
+    $data['directores'] = $directores;
+    $data['reparto'] = $reparto;
+    $data['generos'] = $generos;
+    $data['paises'] = $paises;
+    
+    $this->load->view('peniculas/ficha', $data, TRUE);
+     
+  }
+  
 }
