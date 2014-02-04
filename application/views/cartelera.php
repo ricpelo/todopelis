86cb18547c9116d8c2f35baeedc7cae764088498
Souto <juanjose.souto.ramos@iesdonana.org>
2014-02-04 16:46:59 +0100

    Estrenos en Cine ALPHA

--- application/views/cartelera.php
+++ application/views/cartelera.php
@@ -18,5 +18,19 @@
 	    </div>
 	  <?php endforeach;?>
 	</div>
+
+	<h2>Estrenos en cine</h2>
+	<div>
+	  <?php foreach ($peniculas as $penicula): ?>
+	    <div class="penicula" style="float: left; border: 1px solid black;">
+	      <h3><?= $penicula['titulo']?></h3>
+	      <?php
+	      $cartel = $penicula['cartel']; 
+	      $imagen = array('src' => "$cartel", 'height' => '250', 'width' => '200'); ?>
+	      <?= img($imagen) ?>
+	      <p><?= $penicula['estreno']?></p>
+	    </div>
+	  <?php endforeach;?>
+	</div>
 </body>
 </html>
