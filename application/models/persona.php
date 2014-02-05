602a9bebf49150c3d375c4e00d68caa1bff98dbc
Bornay <Bornay85@gmail.com>
2014-02-05 15:41:14 +0100

    modificacion del modelo actor.php por persona.php

---
+++ application/models/persona.php
@@ -0,0 +1,18 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Persona extends CI_Model
+{
+  function todos($where = "true", $valores = array())
+  {
+    $res = $this->db->query("select * 
+                               from personas 
+                              where ?", $valores);
+    return $res->result_array();
+  }
+
+  function por_nombre($nombre)
+  {
+    return $this->todos("nombre like '%' || ? || '%'", array($nombre));
+  }
+}
+
