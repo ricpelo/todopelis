af7606f2239f38741a655915feb1a3832d492ac2
Chui Ruiz <chuiruiz@hotmail.com>
2014-02-11 16:07:40 +0100

    arrejuntando v3

--- application/views/personas/ficha.php
+++ application/views/personas/ficha.php
@@ -1,6 +1,6 @@
 <?php ob_start(); ?>
 <section id="Persona">
-  <h2 id="nombre"><?= $datos["nombre"] ?></h2>
+  <h2 id="nombre">hola<?= $datos["id"] ?></h2>
   <div class="fila_ficha">
     <div class="ano_nac">Año de nacimiento:</div>
     <div class="dato_fila"><?= $datos['ano'] ?></div>
