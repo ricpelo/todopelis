5449b8169d1f6480716866724f9eb2313d35f996
Souto <juanjose.souto.ramos@iesdonana.org>
2014-02-12 16:33:31 +0100

    cambios en login y alta

--- application/controllers/portal/usuarios.php
+++ application/controllers/portal/usuarios.php
@@ -9,7 +9,7 @@ class Usuarios extends CI_Controller
     $c = $this->uri->segment(2);
     $m = $this->uri->segment(3);
 
-    if ($d != 'portal' || $c != 'usuarios' || $m != 'login')
+    if ($d != 'portal' || $c != 'usuarios' || $m != 'login' && $m != 'alta')
     {
      	if (!$this->Usuario->logueado())
       {
