772213d92ffcb34abcff11ab2f84c090ae7adf28
José María Romero Raposo <jmaria.romero@gmail.com>
2014-02-14 20:56:32 +0100

    otro error

--- application/views/generos/modificar.php
+++ application/views/generos/modificar.php
@@ -3,6 +3,6 @@
 <?= validation_errors();?>
 <?= form_open("admin/generos/modificar/$id"); ?>
   <?= form_label("Nombre", "nombre"); ?>
-  <?= form_input("nombre", $genero['nombre']); ?>
+  <?= form_input("nombre", $nombre); ?>
   <?= form_submit("modificar", "Modificar") ?>
 <?= form_close(); ?>
